09f9791690713bed1ea979b13b396c9eb46d8359
resr <vaclavsr@post.cz>
2023-09-17 17:46:40 +0200

    Finalized the project

--- src/app/components/quizgame/quizgame.component.ts
+++ src/app/components/quizgame/quizgame.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Flag } from 'src/app/models/Flag';
 import { flagsEasy, flagsExpert, flagsExtreme, flagsHard, flagsMedium } from 'src/app/models/flags-data';
 
@@ -11,10 +11,17 @@ import { flagsEasy, flagsExpert, flagsExtreme, flagsHard, flagsMedium } from 'sr
 export class QuizgameComponent implements OnInit {
   difficulty?: string;
   flags: Flag[] = [];
+  shuffledOptions: Flag[][] = [];
+  currentFlagIndex: number = 0;
+  correctAnswers: number = 0;
+  showResults: boolean = false;
+  timer: any;
+  timeLimit: number = 20; 
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
+    this.startTimer();
     this.route.params.subscribe(params => {
       this.difficulty = params['difficulty'];
 
@@ -35,6 +42,83 @@ export class QuizgameComponent implements OnInit {
           this.flags = flagsExtreme;
           break;
       }
+
+      this.shuffleFlags();
     });
+  }
+
+  ngOnDestroy() {
+    this.stopTimer();
+  }
+
+  shuffleFlags() {
+    this.flags = this.shuffleArray(this.flags).slice(0, 10); 
+    this.shuffleOptions();
+  }
+
+  shuffleOptions() {
+    for (const flag of this.flags) {
+      const options = this.generateOptions(flag, this.flags);
+      this.shuffleArray(options);
+      this.shuffledOptions.push(options);
+    }
+  }
+
+  generateOptions(correctOption: Flag, allOptions: Flag[]): Flag[] {
+    const options = [correctOption];
+    while (options.length < 4) {
+      const randomOption = allOptions[Math.floor(Math.random() * allOptions.length)];
+      if (!options.includes(randomOption)) {
+        options.push(randomOption);
+      }
+    }
+    return options;
+  }
+
+  shuffleArray(array: any[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  checkAnswer(selectedOption: Flag | null, correctOption: Flag) {
+    if (selectedOption === correctOption) {
+      this.correctAnswers++;
+    }
+  
+    if (selectedOption !== null) {
+      this.stopTimer();
+    }
+  
+    if (this.currentFlagIndex === this.flags.length - 1) {
+      this.showResults = true;
+    } else {
+      this.currentFlagIndex++;
+      this.startTimer();
+    }
+  }
+
+  finishQuiz() {
+    this.router.navigate(['/']);
+  }
+
+  startTimer() {
+    this.timeLimit = 20;
+    this.timer = setInterval(() => {
+      this.timeLimit--;
+      if (this.timeLimit === 0) {
+        this.handleTimeout();
+      }
+    }, 1000);
+  }
+
+  stopTimer() {
+    clearInterval(this.timer);
+  }
+
+  handleTimeout() {
+    this.checkAnswer(null, this.flags[this.currentFlagIndex]);
   }
 }
